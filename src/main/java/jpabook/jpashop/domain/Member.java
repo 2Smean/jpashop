435e44b0f03ad51b5b4966741dc72367248ba653
2Smean <136636643+2Smean@users.noreply.github.com>
2024-12-31 18:41:20 +0900

    add : Things to keep in mind when designing entities
    - XToOne은 모두 Lazy로 변경
    - 연관 관계 편의 메서드

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -22,7 +22,4 @@ public class Member {
 
     @OneToMany
     private List<Order> orders = new ArrayList<>();
-
-
-
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.*;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -17,18 +19,36 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate; // 주문시간
 
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    //==연관 관계 편의 메서드==//
+    //위치는 핵심적으로 컨트롤하는 곳이 가지고 있는다
+    //양방향일 때 사용하기 좋다
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
