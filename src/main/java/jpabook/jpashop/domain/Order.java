e6c790f2374dc68c768ca5f6196c113532528b57
2Smean <136636643+2Smean@users.noreply.github.com>
2025-01-03 00:20:41 +0900

    add : item domain development ( Order, OrderItem Entity development )

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -32,7 +32,7 @@ public class Order {
 
     private LocalDateTime orderDate; // 주문시간
 
-    private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+    private OrderStatus  status; // 주문상태 [ORDER, CANCEL]
 
     //==연관 관계 편의 메서드==//
     //위치는 핵심적으로 컨트롤하는 곳이 가지고 있는다
@@ -50,5 +50,51 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    //stream 이나 lambda를 사용하면 깔끔하게 정리할 수 있다
+    //        return orderItems.stream()
+    //                .mapToInt(OrderItem::getTotalPrice)
+    //                .sum();
+    public int getTotalPrice() {
+
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
