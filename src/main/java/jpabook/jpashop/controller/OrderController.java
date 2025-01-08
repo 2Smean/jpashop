920f992e7290b786e91f68ac3a89f741eaea22e0
2Smean <136636643+2Smean@users.noreply.github.com>
2025-01-08 19:00:30 +0900

    feat : Web Layer Development ( item Order )

---
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+        // controller 에서 값들을 찾아서 넣을 수도 있지만 Transaction 안에서 JPA가 동작할 때 가장 깔끔하고
+        // 그렇기에 Test 할 때 깔끔하고, Service 가 entity 에 의존성이 더욱 커지고 할 수 있는게 더 많아진다. 
+        orderService.order(memberId, itemId, count); //코드 리팩토링 : 하나의 상품이 아닌 멀티로 수정 가능
+        return "redirect:/orders";
+    }
+}
