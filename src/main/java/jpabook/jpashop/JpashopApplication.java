bc942161f0f14570f7428d073db272b8ee8be73a
2Smean <136636643+2Smean@users.noreply.github.com>
2025-01-14 12:13:52 +0900

    feat : Simple Order Lookup V1: Expose entities directly
    - hivernate5module 을 사용하기 보다 Dto로 변환해서 반환하는 것이 권장된다.
    - 지연로딩(Lazy)를 기본으로 하고 성능 최적화가 필요한 경우에는 fetch join을 사용하자

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,13 +1,25 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
 
 	public static void main(String[] args) {
         SpringApplication.run(JpashopApplication.class, args);
+	}
+
+	// Hibernate5Module 을 사용하기 보다 엔티티를 노출하지 않는 것이 훨씬 좋다.
+	@Bean
+	Hibernate5JakartaModule hibernate5JakartaModule() {
+		Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
+		//강제 지연 로딩 설정
+		//hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5JakartaModule;
 	}
 
 }
